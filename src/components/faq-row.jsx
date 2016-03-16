806978d4972c57a18b989333415207fcb645294a
Minwei Xu <faceswilliam@gmail.com>
2016-03-16 01:26:56 -0700

    array key added

--- src/components/faq-row.jsx
+++ src/components/faq-row.jsx
@@ -1,11 +1,13 @@
 const React = require('react');
 const FaqCard = require('./faq-card.jsx');
+let key = 0;
 
 const FaqRow = React.createClass({
 
   render() {
     const cards = this.props.cards['faqrow' + this.props.row].map(function(obj){
-      return (<FaqCard title={obj.title} body={obj.body} />);
+      key++;
+      return (<FaqCard key={key} title={obj.title} body={obj.body} />);
     });
 
     return (
