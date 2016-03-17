30866675bd1f29c71ed8959992f3c5662d80f9b1
Minwei Xu <faceswilliam@gmail.com>
2016-03-17 00:21:23 -0700

    lint

--- src/components/faq-card.jsx
+++ src/components/faq-card.jsx
@@ -6,31 +6,6 @@ const FaqCard = React.createClass({
     body: React.PropTypes.string.isRequired,
   },
   render() {
-    // var cardStyle = {
-    //   fontFamily: "Proxima Nova, Open Sans, sans-serif",
-    //   marginBottom: "20px",
-    //   backgroundColor: "white",
-    //   borderRadius: "4px",
-    //   color: "#3E3E3E",
-    //   padding: "40px 30px",
-    //   position: "relative",
-    //   textAlign: "left",
-    //   width: "100%"
-    // };
-    //
-    // var cardTitleStyle = {
-    //   fontSize: "20px",
-    //   lineHeight: 1.5,
-    //   marginBottom: "25px",
-    //   fontWeight: "bold",
-    // };
-    //
-    // var cardBodyStyle = {
-    //   fontSize: "14px",
-    //   lineHeight: 1.7,
-    //   fontWeight: 400
-    // };
-
     return (
       <div className="faq-card">
         <div className="card-title">{this.props.title}</div>
