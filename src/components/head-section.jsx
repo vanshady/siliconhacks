d137628af6a3a1ab7506414f429a9f25252e5402
Minwei Xu <faceswilliam@gmail.com>
2016-03-19 00:34:02 -0700

    Apply&Sponsor Us

--- src/components/head-section.jsx
+++ src/components/head-section.jsx
@@ -14,8 +14,8 @@ const HeadSection = React.createClass({
             <div className="sub-headline">Calling all hackers. Dreamers. 4-A.M'ers</div>
             <div className="sub-headline mini">UC Berkeley | Oct. 9-11</div>
             <div className="button-container">
-              <a className="button live" href="live">Live</a>
-              <a className="button" href="http://meerkatapp.co/calhacks">Stream</a>
+              <a className="button" href="live">Apply</a>
+              <a className="button" href="">Sponsor Us</a>
             </div>
           </div>
         </div>
