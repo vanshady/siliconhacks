92e2eded8f1a107a8418993ccd9716d16217385f
Minwei Xu <faceswilliam@gmail.com>
2016-04-06 00:58:51 -0700

    Typo corrected

--- src/components/apply-section.jsx
+++ src/components/apply-section.jsx
@@ -30,7 +30,7 @@ const ApplySection = React.createClass({
       <div id="apply" className="section">
         <div className="desktop-img"></div>
         <div className="apply-container">
-          <div className="apply-header">Join the maling list</div>
+          <div className="apply-header">Join the mailing list</div>
           <a className="button apply" href="https://siliconhacks.typeform.com/to/lzxr3H" target="_blank">Apply</a>
         </div>
       </div>
