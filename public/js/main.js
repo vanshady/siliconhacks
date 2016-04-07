2ba7a2b638f10d71b92ada8c7f82123aec805e55
Minwei Xu <faceswilliam@gmail.com>
2016-04-06 22:35:38 -0700

    banner and logo

--- public/js/main.js
+++ public/js/main.js
@@ -24573,8 +24573,8 @@ const Nav = React.createClass({
     return React.createElement(
       "nav",
       { id: "nav" },
-      React.createElement("img", { id: "logo_white", className: "logo", src: "assets/img/logo_white.svg", alt: "Calhacks" }),
-      React.createElement("img", { id: "logo_orange", className: "logo", src: "assets/img/logo_orange.svg", alt: "Calhacks" }),
+      React.createElement("img", { id: "logo_white", className: "logo", src: "assets/img/logo_green.png", alt: "Calhacks" }),
+      React.createElement("img", { id: "logo_orange", className: "logo", src: "assets/img/logo_green.png", alt: "Calhacks" }),
       React.createElement("img", { id: "menu_white", className: "nav-menu", src: "assets/img/menu_white.svg", alt: "" }),
       React.createElement("img", { id: "menu_orange", className: "nav-menu", src: "assets/img/menu_orange.svg", alt: "" }),
       React.createElement(
--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -4,8 +4,8 @@ const Nav = React.createClass({
   render() {
     return (
       <nav id="nav">
-        <img id="logo_white" className="logo" src="assets/img/logo_white.svg" alt="Calhacks" />
-        <img id="logo_orange" className="logo" src="assets/img/logo_orange.svg" alt="Calhacks" />
+        <img id="logo_white" className="logo" src="assets/img/logo_green.png" alt="Calhacks" />
+        <img id="logo_orange" className="logo" src="assets/img/logo_green.png" alt="Calhacks" />
         <img id="menu_white" className="nav-menu" src="assets/img/menu_white.svg" alt="" />
         <img id="menu_orange" className="nav-menu" src="assets/img/menu_orange.svg" alt="" />
         <div id="nav-sections">
