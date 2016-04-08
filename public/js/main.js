53f99d8676d32dc7527d55a4ee979b074c4ad354
Minwei Xu <faceswilliam@gmail.com>
2016-04-07 22:58:20 -0700

    logo_green

--- public/js/main.js
+++ public/js/main.js
@@ -24573,8 +24573,8 @@ const Nav = React.createClass({
     return React.createElement(
       "nav",
       { id: "nav" },
-      React.createElement("img", { id: "logo_white", className: "logo", src: "assets/img/logo_white.png", alt: "Calhacks" }),
-      React.createElement("img", { id: "logo_orange", className: "logo", src: "assets/img/logo_orange.png", alt: "Calhacks" }),
+      React.createElement("img", { id: "logo_white", className: "logo", src: "assets/img/logo_white.png", alt: "SiliconHacks" }),
+      React.createElement("img", { id: "logo_green", className: "logo", src: "assets/img/logo_green.png", alt: "SiliconHacks" }),
       React.createElement("img", { id: "menu_white", className: "nav-menu", src: "assets/img/menu_white.svg", alt: "" }),
       React.createElement("img", { id: "menu_orange", className: "nav-menu", src: "assets/img/menu_orange.svg", alt: "" }),
       React.createElement(
--- public/js/script.js
+++ public/js/script.js
@@ -25,11 +25,11 @@ var switchNav = function () {
   if ($(window).scrollTop() > 15) {
     $('#nav').addClass('active');
     $('#logo_white').hide();
-    $('#logo_orange').show();
+    $('#logo_green').show();
   } else {
     $('#nav').removeClass('active');
     $('#logo_white').show();
-    $('#logo_orange').hide();
+    $('#logo_green').hide();
   }
 };
 
@@ -37,11 +37,11 @@ var switchNavMobile = function () {
   if ($(window).scrollTop() > 15) {
     $('#nav').addClass('active-mobile');
     $('#logo_white').hide();
-    $('#logo_orange').show();
+    $('#logo_green').show();
   } else {
     $('#nav').removeClass('active-mobile');
     $('#logo_white').show();
-    $('#logo_orange').hide();
+    $('#logo_green').hide();
   }
 };
 
@@ -51,29 +51,6 @@ var activateSection = function (section) {
   $(section).addClass('active');
 };
 
-var poem = '%cCal Hacks, a poem %c\n\n' +
-
-  '%cThere once was a wee young lad,\n' +
-  'whose brain had gone all sad,\n' +
-  "for he didn't ignite his thoughts or his bytes,\n" +
-  "and now his friends laugh cause his life isn't rad.\n" +
-  'So one dark, stormy night,\n he found, to his delight,\n' +
-  'a hackathon so, extravagant, bold,\n' +
-  'that he giggled with glee as he hit apply.\n' +
-  'And now with his acceptance,\n his friends are filled with reverence,\n' +
-  'they pleaded and pleaded, for a chance to compete with,\n' +
-  'this lad in Cal Hacks and go undefeated.\n\n' +
-
-  'There once was a bored little lass,\n who wanted to try out SASS,\n' +
-  'so she hit apply and overnight,\n' +
-  'she found her life filled with delight.\n' +
-  'Now everywhere she go,\n she radiates a glow,\n' +
-  "cause she's full of ideas of uses and features,\n" +
-  'for all types of users and all types of creatures.\n\n' +
-
-  'And now for the final guest,\n a hacker with wonder and zest,\n' +
-  "who doesn't deplore a chance to explore,\n" +
-  'come join us for 2 days of hacking galore!';
 
 $(document).ready(function () {
 
@@ -95,7 +72,7 @@ $(document).ready(function () {
   } else {
     $('#nav').addClass('active-mobile');
     $('#logo_white').hide();
-    $('#logo_orange').show();
+    $('#logo_green').show();
     $('#nav-close, .nav-section').click(function () {
       $('#nav-sections').removeClass('active');
       setTimeout(function () {
