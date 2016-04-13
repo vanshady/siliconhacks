3ed146e8501e9a6ec4a1e4a69c7c6c5de453f37c
Minwei Xu <faceswilliam@gmail.com>
2016-04-12 17:48:21 -0700

    404 handler

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const app = express();
+var bodyParser = require('body-parser');
 
 // const port = process.env.VCAP_APP_PORT || 3000;
 const port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(function(req, res, next) {
+  res.status(404).sendFile(__dirname + '/public/error.html');
+});
 
 app.get('/', function (req, res) {
   res.render('index');
