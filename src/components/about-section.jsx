dbc35b0251e655bcfcfa37470f5a1e7db8de7303
Minwei Xu <faceswilliam@gmail.com>
2017-02-28 23:29:59 -0500

    update

--- src/components/about-section.jsx
+++ src/components/about-section.jsx
@@ -7,7 +7,7 @@ const AboutSection = React.createClass({
         <div className="section-wrapper">
           <div className="section-body">
             <h1>
-              Come Hack With The Best
+              Come Hack With The Best For Social Impact
             </h1>
             <p>Turn your energy and free food into awesome projects by the end of one weekend. Bring your laptop, passion, smartphone, and whatever else you can manage to carry to this once-in-a-lifetime event and have some fun working alongside some of the best minds in Silicon Valley for social impact!</p>
           </div>
