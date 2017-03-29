741249b359c4f9d5b6d999cd0dfd9dcc2411632c
Minwei Xu <faceswilliam@gmail.com>
2017-03-29 02:48:27 -0400

    update

--- src/components/about-section.jsx
+++ src/components/about-section.jsx
@@ -9,7 +9,7 @@ const AboutSection = React.createClass({
             <h1>
               Come Hack With The Best For Social Impact
             </h1>
-            <p>Turn your energy and free food into awesome projects by the end of one weekend. Bring your laptop, passion, smartphone, and whatever else you can manage to carry to this once-in-a-lifetime event and have some fun working alongside some of the best minds in Silicon Valley for social impact!</p>
+            <p>Turn your energy and free food into awesome projects by the end of one weekend. Bring your laptop, passion, smartphone, and whatever else you can manage to carry to this once-in-a-lifetime event and have some fun working alongside some of the best minds in Silicon Valley for social impact! We'll have 3 main tracks: Education, Civic Engagement, and Health/Fitness.</p>
           </div>
         </div>
         <div className="slant bottom left"></div>
