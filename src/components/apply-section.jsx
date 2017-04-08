fc55aed26aa8db851372728d14254d80ca1a6971
Minwei Xu <faceswilliam@gmail.com>
2017-04-08 12:33:29 -0400

    close application

--- src/components/apply-section.jsx
+++ src/components/apply-section.jsx
@@ -6,8 +6,8 @@ const ApplySection = React.createClass({
       <div id="apply" className="section">
         <div className="desktop-img"></div>
         <div className="apply-container">
-          <div className="apply-header">Applications are now open!</div>
-          <a className="button apply" href="https://siliconhacks.typeform.com/to/wK5Mls" target="_blank">Apply</a>
+          <div className="apply-header">Applications are closed.</div>
+          <a className="button apply">Application Closed</a>
         </div>
       </div>
     );
