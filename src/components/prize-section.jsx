00e6997b7afc2837f48604ee65c83ee351f27903
Minwei Xu <faceswilliam@gmail.com>
2017-05-03 00:29:02 -0400

    prize $

--- src/components/prize-section.jsx
+++ src/components/prize-section.jsx
@@ -9,6 +9,9 @@ const PrizeSection = () => (
             </h1>
             <br />
             <h3>
+              We have prizes that are worth <strong>$25,000</strong> in total😜
+            </h3>
+            <h3>
               Please check out our page at <a href="https://siliconhacks.hackerearth.com/#prizes" target="_blank">HackerEarth</a>.
             </h3>
           </div>
